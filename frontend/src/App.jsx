b565b370fb6517d6a9eae11aaaa55dfe1f75410e
Pamalka Nethmani <153271780+Nethmani@users.noreply.github.com>
2024-07-15 18:24:59 +0530

    start creating a frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div calssName="app">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
