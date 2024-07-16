2b441e1a804e09b244e831303380ce1cc9c94334
Pamalka Nethmani <153271780+Nethmani@users.noreply.github.com>
2024-07-16 16:19:49 +0530

    Create Cart,Home,Place order pages

---
+++ frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Cart.css";
+
+const Cart = () => {
+  return <div></div>;
+};
+
+export default Cart;
---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./PlaceOrder.css";
+
+const PlaceOrder = () => {
+  return <div></div>;
+};
+
+export default PlaceOrder;
