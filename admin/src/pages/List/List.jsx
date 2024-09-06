09a4357a5773ac6bc732ef35d342084e1e9ccbb4
Pamalka Nethmani <153271780+Nethmani@users.noreply.github.com>
2024-09-06 12:10:08 +0530

    change

---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./List.css";
+
+const List = () => {
+  return (
+    <div className="add">
+      <form className="flex-col">
+        <div className="add-img-upload flex-col">
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img src={assets.upload_area} alt="" />
+          </label>
+          <input type="file" id="image" hidden required />
+        </div>
+        <div className="add-product-name flex-col">
+          <p>Product name</p>
+          <input type="text" name="name" placeholder="Type here" />
+        </div>
+        <div className="add-product-description flex-col">
+          <p>Product description</p>
+          <textarea
+            name="description"
+            rows="6"
+            placeholder="Write content here"
+            required
+          ></textarea>
+        </div>
+        <div className="add-category-price">
+          <div className="add-category flex-col">
+            <p>Product category</p>
+            <select name="category"></select>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default List;
---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Orders.css";
+
+const Orders = () => {
+  return <div>Orders</div>;
+};
+
+export default Orders;
