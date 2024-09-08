b9743062aa2dcef62abd8e17652db1aaef5bb1f2
Pamalka Nethmani <153271780+Nethmani@users.noreply.github.com>
2024-09-08 21:03:48 +0530

    Create admin panel using react JS

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/assets";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const Add = () => {
   const url = "http://localhost:4000";
@@ -36,7 +37,9 @@ const Add = () => {
         category: "Salad",
       });
       setImage(false);
+      toast.success(response.data.message);
     } else {
+      toast.error(response.data.message);
     }
   };
   return (
--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,37 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
+  const url = "http://localhost:4000";
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+
+      if (response.data.success) {
+        setList(response.data.data);
+      } else {
+        toast.error("Error fetching the list");
+      }
+    } catch (error) {
+      toast.error("Error fetching the list");
+    }
+  };
+
+  const removeFood = async (foodId) => {
+    const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return (
-    <div className="add">
-      <form className="flex-col">
-        <div className="add-img-upload flex-col">
-          <p>Upload Image</p>
-          <label htmlFor="image">
-            <img src={assets.upload_area} alt="" />
-          </label>
-          <input type="file" id="image" hidden required />
+    <div className="list add flex-col">
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
         </div>
-        <div className="add-product-name flex-col">
-          <p>Product name</p>
-          <input type="text" name="name" placeholder="Type here" />
-        </div>
-        <div className="add-product-description flex-col">
-          <p>Product description</p>
-          <textarea
-            name="description"
-            rows="6"
-            placeholder="Write content here"
-            required
-          ></textarea>
-        </div>
-        <div className="add-category-price">
-          <div className="add-category flex-col">
-            <p>Product category</p>
-            <select name="category"></select>
-          </div>
-        </div>
-      </form>
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/` + item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>{item.price}</p>
+              <p onClick={() => removeFood(item._id)} className="cursor">
+                X
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
