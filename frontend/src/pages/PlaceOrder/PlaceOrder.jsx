fdc204d8056bc6631cf2534b0372de52652fee50
Pamalka Nethmani <153271780+Nethmani@users.noreply.github.com>
2025-01-28 10:46:02 +0530

    fix order page bug

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
